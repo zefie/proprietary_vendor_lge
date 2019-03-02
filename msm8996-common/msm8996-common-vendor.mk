4597b66f536d4dcdb00d305efab3ab26960cc032
Michael <wolfovelp@gmail.com>
2019-03-02 21:00:39 +0100

    AOD: Bringup LGE AOD requirements
    by Imaginovskiy

--- msm8996-common/msm8996-common-vendor.mk
+++ msm8996-common/msm8996-common-vendor.mk
@@ -598,6 +598,41 @@ PRODUCT_COPY_FILES += \
     vendor/lge/msm8996-common/proprietary/vendor/lib64/libhui.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhui.so \
     vendor/lge/msm8996-common/proprietary/vendor/lib64/libux.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libux.so
 
+# AOD
+PRODUCT_COPY_FILES += \
+    vendor/lge/msm8996-common/proprietary/vendor/bin/hw/vendor.lge.hardware.aod.aodview@1.0-service:$(TARGET_COPY_OUT_VENDOR)/bin/hw/vendor.lge.hardware.aod.aodview@1.0-service \
+    vendor/lge/msm8996-common/proprietary/vendor/bin/hw/vendor.lge.hardware.aod.aodcommon@1.0-service:$(TARGET_COPY_OUT_VENDOR)/bin/hw/vendor.lge.hardware.aod.aodcommon@1.0-service \
+    vendor/lge/msm8996-common/proprietary/vendor/bin/hw/vendor.lge.hardware.aod.aodpower@1.0-service:$(TARGET_COPY_OUT_VENDOR)/bin/hw/vendor.lge.hardware.aod.aodpower@1.0-service \
+    vendor/lge/msm8996-common/proprietary/vendor/etc/init/vendor.lge.hardware.aod.aodcommon@1.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.lge.hardware.aod.aodcommon@1.0-service.rc \
+    vendor/lge/msm8996-common/proprietary/vendor/etc/init/vendor.lge.hardware.aod.aodpower@1.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.lge.hardware.aod.aodpower@1.0-service.rc \
+    vendor/lge/msm8996-common/proprietary/vendor/etc/init/vendor.lge.hardware.aod.aodview@1.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.lge.hardware.aod.aodview@1.0-service.rc \
+    vendor/lge/msm8996-common/proprietary/vendor/lib64/vendor.lge.hardware.aod.aodcommon@1.0_vendor.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.lge.hardware.aod.aodcommon@1.0_vendor.so \
+    vendor/lge/msm8996-common/proprietary/vendor/lib64/vendor.lge.hardware.aod.aodview@1.0_vendor.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.lge.hardware.aod.aodview@1.0_vendor.so \
+    vendor/lge/msm8996-common/proprietary/vendor/lib64/vendor.lge.hardware.aod.aodpower@1.0_vendor.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.lge.hardware.aod.aodpower@1.0_vendor.so \
+    vendor/lge/msm8996-common/proprietary/lib64/vendor.lge.hardware.aod.aodview@1.0.so:/system/lib64/vendor.lge.hardware.aod.aodview@1.0.so \
+    vendor/lge/msm8996-common/proprietary/lib64/libaodlayer.so:/system/lib64/libaodlayer.so \
+    vendor/lge/msm8996-common/proprietary/lib64/vendor.lge.hardware.aod.aodpower@1.0.so:/system/lib64/vendor.lge.hardware.aod.aodpower@1.0.so \
+    vendor/lge/msm8996-common/proprietary/lib64/vendor.lge.hardware.aod.aodcommon@1.0.so:/system/lib64/vendor.lge.hardware.aod.aodcommon@1.0.so \
+    vendor/lge/msm8996-common/proprietary/lib/vendor.lge.hardware.aod.aodview@1.0.so:/system/lib/vendor.lge.hardware.aod.aodview@1.0.so \
+    vendor/lge/msm8996-common/proprietary/lib/libaodlayer.so:/system/lib/libaodlayer.so \
+    vendor/lge/msm8996-common/proprietary/lib/vendor.lge.hardware.aod.aodpower@1.0.so:/system/lib/vendor.lge.hardware.aod.aodpower@1.0.so \
+    vendor/lge/msm8996-common/proprietary/lib/vendor.lge.hardware.aod.aodcommon@1.0.so:/system/lib/vendor.lge.hardware.aod.aodcommon@1.0.so \
+    vendor/lge/msm8996-common/proprietary/framework/vendor.lge.hardware.aod.aodpower-V1.0-java.jar:/system/framework/vendor.lge.hardware.aod.aodpower-V1.0-java.jar \
+    vendor/lge/msm8996-common/proprietary/framework/oat/arm/vendor.lge.hardware.aod.aodview-V1.0-java.vdex:/system/framework/oat/arm/vendor.lge.hardware.aod.aodview-V1.0-java.vdex \
+    vendor/lge/msm8996-common/proprietary/framework/oat/arm/vendor.lge.hardware.aod.aodpower-V1.0-java.odex:/system/framework/oat/arm/vendor.lge.hardware.aod.aodpower-V1.0-java.odex \
+    vendor/lge/msm8996-common/proprietary/framework/oat/arm/vendor.lge.hardware.aod.aodcommon-V1.0-java.odex:/system/framework/oat/arm/vendor.lge.hardware.aod.aodcommon-V1.0-java.odex \
+    vendor/lge/msm8996-common/proprietary/framework/oat/arm/vendor.lge.hardware.aod.aodpower-V1.0-java.vdex:/system/framework/oat/arm/vendor.lge.hardware.aod.aodpower-V1.0-java.vdex \
+    vendor/lge/msm8996-common/proprietary/framework/oat/arm/vendor.lge.hardware.aod.aodview-V1.0-java.odex:/system/framework/oat/arm/vendor.lge.hardware.aod.aodview-V1.0-java.odex \
+    vendor/lge/msm8996-common/proprietary/framework/oat/arm/vendor.lge.hardware.aod.aodcommon-V1.0-java.vdex:/system/framework/oat/arm/vendor.lge.hardware.aod.aodcommon-V1.0-java.vdex \
+    vendor/lge/msm8996-common/proprietary/framework/oat/arm64/vendor.lge.hardware.aod.aodview-V1.0-java.vdex:/system/framework/oat/arm64/vendor.lge.hardware.aod.aodview-V1.0-java.vdex \
+    vendor/lge/msm8996-common/proprietary/framework/oat/arm64/vendor.lge.hardware.aod.aodpower-V1.0-java.odex:/system/framework/oat/arm64/vendor.lge.hardware.aod.aodpower-V1.0-java.odex \
+    vendor/lge/msm8996-common/proprietary/framework/oat/arm64/vendor.lge.hardware.aod.aodcommon-V1.0-java.odex:/system/framework/oat/arm64/vendor.lge.hardware.aod.aodcommon-V1.0-java.odex \
+    vendor/lge/msm8996-common/proprietary/framework/oat/arm64/vendor.lge.hardware.aod.aodpower-V1.0-java.vdex:/system/framework/oat/arm64/vendor.lge.hardware.aod.aodpower-V1.0-java.vdex \
+    vendor/lge/msm8996-common/proprietary/framework/oat/arm64/vendor.lge.hardware.aod.aodview-V1.0-java.odex:/system/framework/oat/arm64/vendor.lge.hardware.aod.aodview-V1.0-java.odex \
+    vendor/lge/msm8996-common/proprietary/framework/oat/arm64/vendor.lge.hardware.aod.aodcommon-V1.0-java.vdex:/system/framework/oat/arm64/vendor.lge.hardware.aod.aodcommon-V1.0-java.vdex \
+    vendor/lge/msm8996-common/proprietary/framework/vendor.lge.hardware.aod.aodview-V1.0-java.jar:/system/framework/vendor.lge.hardware.aod.aodview-V1.0-java.jar \
+    vendor/lge/msm8996-common/proprietary/framework/vendor.lge.hardware.aod.aodcommon-V1.0-java.jar:/system/framework/vendor.lge.hardware.aod.aodcommon-V1.0-java.jar
+
 PRODUCT_PACKAGES += \
     com.quicinc.cne.api@1.0 \
     com.quicinc.cne.constants@1.0 \
