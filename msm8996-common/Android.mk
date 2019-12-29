0dd0975b71f5b4cbc28fd1294f90b6a80ebb0557
dom133 <dom133_pl@protonmail.com>
2019-12-29 14:40:37 +0000

    msm8996-common: Fix WiFi Display

--- msm8996-common/Android.mk
+++ msm8996-common/Android.mk
@@ -419,15 +419,5 @@ LOCAL_MODULE_SUFFIX := .apk
 LOCAL_PRIVILEGED_MODULE := true
 include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := WfdCommon
-LOCAL_MODULE_OWNER := lge
-LOCAL_SRC_FILES := proprietary/framework/WfdCommon.jar
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := JAVA_LIBRARIES
-LOCAL_MODULE_SUFFIX := .jar
-include $(BUILD_PREBUILT)
-
 endif
 endif
